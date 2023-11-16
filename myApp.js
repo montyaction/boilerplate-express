3154bb67b0c481200a1bcec767bd5f9443b874b5
montyaction <montyaction@hotmail.com>
2023-11-16 17:13:10 +0530

    Add some code and file for deploy in netlify

--- myApp.js
+++ myApp.js
@@ -1,6 +1,6 @@
 let express = require('express');
 let app = express();
-
+require('dotenv').config();
 
 
 
@@ -13,11 +13,30 @@ app.get("/", (req, res) => {
     res.send("Hello Express");
 });
 
-
 // #3 Serve an HTML File
+app.get("/html", (req, res) => {
+    res.sendFile(__dirname + '/views/index.html');
+});
+
 // #4 Serve Static Assets
+app.use("/public", express.static(__dirname + "/public"));
+
 // #5 Serve JSON on a Specific Route
+app.get("/json", (req, res) => {
+    res.json({
+        "message": "Hello json"
+    });
+});
+
 // #6 Use the .env File
+app.get("/json1", (req, res) => {
+    if (process.env['MESSAGE_STYLE'] == "uppercase") {
+        res.json({"message": "HELLO WORLD"});
+    } else {
+        res.json({"message": "Hello world"});
+    }
+});
+
 // #7 Implement a Root-Level Request Logger Middleware
 // #8 Chain Middleware to Create a Timer Server
 // #9 Get Route Parameter Input from the Client
